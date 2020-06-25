f4117a325b4c4748f8b7daf62158654d82436b66
tiecmu <62679230+tiecmu@users.noreply.github.com>
2020-06-25 03:01:25 -0700

    scikit-learn 라이브러리 설치여부 확인후 미설치시 설치

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ SETUP_DIR="./project"
 WEB_DIR="$SETUP_DIR/"templates
 ETC_DIR="$SETUP_DIR/"static
 
-ELA_DIR="./elasticsearch-7.6.2"
 
 if [ ! -d "$SETUP_DIR" ]; then
 	mkdir -p $WEB_DIR
@@ -16,21 +15,14 @@ cp home.html $WEB_DIR
 cp word_analysis.html $WEB_DIR
 cp cos_sim.html $WEB_DIR
 
-if [ ! -d "$ELA_DIR" ]; then
-	echo "ERROR : Elasticsearch을 찾을 수 없습니다. 현재 위치에 elasticsearch-7.6.2 폴더가 있는지 확인해 주세요."
-else
-	cd $ELA_DIR/bin/
-	./elasticsearch &
-	cd
-fi
+python -c 'import subprocess
+import sys
+try:
+	import sklearn
+except ModuleNotFoundError:
+	subprocess.call([sys.executable, "-m", "pip", "install", "scikit-learn"])'
+	
 
 cd $SETUP_DIR
 
 ./app.py
-
-PROCESS=`ps -ef | grep elasticsearch`
-
-PID=`echo $PROCESS | cut -f 2 -d' '`
-
-kill -9 $PID
-
