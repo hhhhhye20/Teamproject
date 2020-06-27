73e3fc01d281b41d2a41b40070c68bd7922f76cd
Jaeseong4156 <wotjd2679@gmail.com>
2020-06-27 17:05:27 +0900

    이미지 폴더 생성 및 파일 복사를 위한 쉘 파일 수정

--- setup.sh
+++ setup.sh
@@ -3,17 +3,18 @@ SETUP_DIR="./project"
 
 WEB_DIR="$SETUP_DIR/"templates
 ETC_DIR="$SETUP_DIR/"static
-
+IMG_DIR="$IMG_DIR/"img
 
 if [ ! -d "$SETUP_DIR" ]; then
 	mkdir -p $WEB_DIR
-	mkdir $ETC_DIR
+	mkdir -p $IMG_DIR
 fi
 
 cp app.py $SETUP_DIR
 cp home.html $WEB_DIR
 cp word_analysis.html $WEB_DIR
 cp cos_sim.html $WEB_DIR
+cp loading_text.gif $IMG_DIR
 
 python -c 'import subprocess
 import sys
