15828884cc276984f3668164f9c55473f9f69357
Jaeseong4156 <wotjd2679@gmail.com>
2020-06-28 09:01:41 +0900

    css파일을 저장하고 복사하도록 쉘파일 수정

--- setup.sh
+++ setup.sh
@@ -4,10 +4,11 @@ SETUP_DIR="./project"
 WEB_DIR="$SETUP_DIR/"templates
 ETC_DIR="$SETUP_DIR/"static
 IMG_DIR="$ETC_DIR/"img
-
+CSS_DIR="$ETC_DIR/"css
 
 mkdir -p $WEB_DIR
 mkdir -p $IMG_DIR
+mkdir -p $CSS_DIR
 
 
 cp app.py $SETUP_DIR
@@ -15,6 +16,7 @@ cp home.html $WEB_DIR
 cp word_analysis.html $WEB_DIR
 cp cos_sim.html $WEB_DIR
 cp loading_text.gif $IMG_DIR
+cp style_home.css $CSS_DIR
 
 python -c 'import subprocess
 import sys
